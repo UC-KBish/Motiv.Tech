5f4bd76518fc21cf84b8d34b7db1f79c4bcb7f56
UC-KBish <bishopkw@mail.uc.edu>
2023-11-04 16:36:18 -0400

    Made database structure

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ New Project
 
 Live Share:
 https://prod.liveshare.vsengsaas.visualstudio.com/join?D8AE0994BBE5F8AD762A54B85A44173D59E0
+
+
+
+
+
+
+
+
+
+
+
+#Algorithm:
+###Content-Based Filtering - Video Information
+More like this mongodb: https://www.mongodb.com/blog/post/new-atlas-search-improve-content-recommendations-more-like-this
+
---
+++ middleman.py
@@ -0,0 +1,51 @@
+from pymongo import MongoClient
+from flask import Flask
+
+client = None
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return str(client)
+
+
+def get_database():
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = (
+        "mongodb+srv://wymotiv:motivteam1234@cluster0.krumakm.mongodb.net/"
+    )
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client["Motiv"]
+
+
+# This is added so that many files can reuse the function get_database()
+if __name__ == "__main__":
+    # Get the database
+    dbname = get_database()
+    collection_name = dbname["Posts"]
+
+    item_1 = {
+        "sport_type": "football",
+        "data": {
+            "Touchdowns": 10
+        }
+    }
+
+    item_2 = {
+        "sport_type": "golf",
+        "data": {
+            "Putts": 8
+        }
+    }
+
+    collection_name.insert_many([item_1, item_2])
+
+if __name__ == '__main__':
+    client = get_database()
+
+    app.run()
